c0c4a05f1d7166cd5dc556ed8602c38aabf328e5
Noel Mier-Luna <noel.lmier@gmail.com>
2026-05-13 20:06:43 -0700

    4th Commit

--- 210-lab-37.cpp
+++ 210-lab-37.cpp
@@ -2,39 +2,51 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <map>
+#include <list>
 using namespace std;
 
-int sum_ascii(string s);
+// Changing to generate hash index.
+int gen_hash_index(const string &s); // Passing string by reference to avoid unnecessary copying and const to prevent modification of the string.
 
-int main() {
+int main()
+{
+    // Create our map. Hash index will be key, list of strings will be value.
+    map<int, list<string>> hashMap;
+
     ifstream infile("lab-37-data-3.txt");
-    if (!infile) { // Check if the file was opened successfully.
+    if (!infile)
+    { // Check if the file was opened successfully.
         cerr << "Error opening file!" << endl;
         return 1;
     }
-    int grantTotal = 0; // Used to accumulate total sum of ASCII values for each string read from the file.
-    while (infile) {
+    while (infile)
+    {
         string str;
         infile >> str;
-        if (infile) { // Check if the read was successful.
-            int asciiSum = sum_ascii(str); // Calculate the sum of ASCII values for the string.
-            grantTotal += asciiSum; // Add the ASCII sum to the grand total.
+        if (infile)
+        {                                        // Check if the read was successful.
+            int hashIndex = gen_hash_index(str); // Generates hash index for the string.
+            hashMap[hashIndex].push_back(str);   // Insert the string into the list associated with the hash index.
         }
     }
-    cout << "Grand Total of ASCII values: " << grantTotal << endl; // Correctly displays grand total of 69893419.
+    infile.close(); // Close the file after reading.
 
     return 0;
 }
-// Returns the sum of the ASCII values of the characters in the string s.
-int sum_ascii(string s) {
+// Now generates a hash index using modulo to ensure the index fits within a certain range.
+int gen_hash_index(const string &s)
+{
+    const int HASH_SIZE = 105; // Define a hash size to limit the range of hash indices. Using a little over 100 for part 3.
     int sum = 0;
-    for (char c : s) {
-        sum += (int) c; // Add the ASCII value of each character to the sum.
+    for (char c : s)
+    {
+        sum += (int)c; // Add the ASCII value of each character to the sum.
     }
-    return sum;
+    return sum % HASH_SIZE; // Return the hash index within the defined range.
 }
 
-/* 
+/*
 These targets are present in the dataset and can be used for testing:
 536B9DFC93AF
 1DA9D64D02A0
