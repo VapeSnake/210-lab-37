0fa08123ab07fce1ebd86d6957e19ba3ce427acf
Noel Mier-Luna <noel.lmier@gmail.com>
2026-05-13 19:42:34 -0700

    Milestone 1

--- 210-lab-37.cpp
+++ 210-lab-37.cpp
@@ -7,29 +7,23 @@ using namespace std;
 int sum_ascii(string s);
 
 int main() {
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
-
     // Testing target test values with sum_ascii function.
     string target1 = "536B9DFC93AF"; // ASCII sum is 729
-    string target2 = "1DA9D64D02A0"; // ASCII sum is 694
-    string target3 = "666D109AA22E"; // ASCII sum is 674
-    string target4 = "E1D2665B21EA"; // ASCII sum is 681
+    string target2 = "1DA9D64D02A0"; // ASCII sum is 692
+    string target3 = "666D109AA22E"; // ASCII sum is 683
+    string target4 = "E1D2665B21EA"; // ASCII sum is 696
 
     cout << "ASCII sum of " << target1 << " is: " << sum_ascii(target1) << endl; // Showed correct ASCII sum for target1.
-    cout << "ASCII sum of " << target2 << " is: " << sum_ascii(target2) << endl; // Showed incorrect ASCII sum.
-    cout << "ASCII sum of " << target3 << " is: " << sum_ascii(target3) << endl; // Showed incorrect ASCII sum.
-    cout << "ASCII sum of " << target4 << " is: " << sum_ascii(target4) << endl; // Showed incorrect ASCII sum. Error caused likely by newline.
+    // I miscounted, sums are correct.
+    cout << "ASCII sum of " << target2 << " is: " << sum_ascii(target2) << endl;
+    cout << "ASCII sum of " << target3 << " is: " << sum_ascii(target3) << endl;
+    cout << "ASCII sum of " << target4 << " is: " << sum_ascii(target4) << endl; 
 
 
 
     return 0;
 }
-
+// Returns the sum of the ASCII values of the characters in the string s.
 int sum_ascii(string s) {
     int sum = 0;
     for (char c : s) {
